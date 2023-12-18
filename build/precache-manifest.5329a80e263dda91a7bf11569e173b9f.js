53bf848a166813fcc5fffdc8a9c7097530fcf644
Divya-Mali <128035416+Divya-Mali@users.noreply.github.com>
2023-12-18 15:39:28 +0530

    Add files via upload

---
+++ build/precache-manifest.5329a80e263dda91a7bf11569e173b9f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca43d52ceffb711976ab5f98bf22d5c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c475ff69d51fe20b4eb",
+    "url": "/static/css/main.ae81e25f.chunk.css"
+  },
+  {
+    "revision": "cd4330cc9459738d417a",
+    "url": "/static/js/2.6e909dd0.chunk.js"
+  },
+  {
+    "revision": "cc3f99056d602258afe7",
+    "url": "/static/js/3.cc8dfcd1.chunk.js"
+  },
+  {
+    "revision": "2c475ff69d51fe20b4eb",
+    "url": "/static/js/main.dc3df3c1.chunk.js"
+  },
+  {
+    "revision": "571a692600e6379471b5",
+    "url": "/static/js/runtime~main.a96156d7.js"
+  },
+  {
+    "revision": "c30aefb9fb45259463ad187d133c17ff",
+    "url": "/static/media/admin.c30aefb9.png"
+  },
+  {
+    "revision": "792fc45fb8be6ecf8257178ae5b6789a",
+    "url": "/static/media/banner.792fc45f.jpg"
+  },
+  {
+    "revision": "11ee7fd2566fb49fa40efad787a76a23",
+    "url": "/static/media/logo.11ee7fd2.png"
+  }
+]);
